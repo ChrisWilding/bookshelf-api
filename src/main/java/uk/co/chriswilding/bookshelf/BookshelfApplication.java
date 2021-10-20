4db851a77c6ed03b4c207f6c26ddb2d547cd1f04
Chris Wilding <hello@chriswilding.co.uk>
2021-10-20 20:19:44 +0100

    Initial commit

---
+++ src/main/java/uk/co/chriswilding/bookshelf/BookshelfApplication.java
@@ -0,0 +1,11 @@
+package uk.co.chriswilding.bookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshelfApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookshelfApplication.class, args);
+    }
+}
---
+++ src/test/java/uk/co/chriswilding/bookshelf/BookshelfApplicationTests.java
@@ -0,0 +1,11 @@
+package uk.co.chriswilding.bookshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshelfApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
